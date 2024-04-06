56c0f6ccd549963984aa6c40fc169d6dd29d33a3
Vince882 <SQ.vincebhingradiya@gmail.com>
2024-04-06 17:41:11 +0530

    change

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std; 
+
+int main()
+{
+    int i,j;
+    cout<<"Enter the number:";
+    cin>>i;
+    cin>>j;
+    cout<<"the sum is :"<<i+j;
+    return 0; 
+}
---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i
+    ;
+    cout<<"Enter the value:";
+    cin>>i;
+    cout<<"Hello world"<<i<<endl;
+    return 0;
+}
